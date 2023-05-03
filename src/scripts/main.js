a0c5b856a4c05200d6a84b09590990bd7dd8aa9e
Sebastián Kravetz <sebastiankravetz@icloud.com>
2023-05-03 00:56:42 -0400

    adding js scripts

---
+++ src/scripts/main.js
@@ -0,0 +1,37 @@
+
+
+
+// validador de formulario de contacto
+
+const contactFormValidator = () => {
+
+    const formElement = document.querySelector("#contact-form");
+
+
+    formElement.addEventListener("submit", (evento) => {
+        evento.preventDefault();
+
+        console.log(evento.target)
+        console.log()
+
+
+    })
+
+
+}
+
+
+// fix para svg externo con animación de path
+const dynamicSvgFix = () => {
+    const svgHolder = document.querySelector('object#dynamic-svg');
+    svgHolder.onload = () => {
+        const svgDocument = svgHolder.contentDocument;
+        const style = svgDocument.createElementNS("http://www.w3.org/2000/svg", "style");
+        style.textContent = '@import url("producto_animado.css");';
+        const svgElem = svgDocument.querySelector('svg');
+        svgElem.insertBefore(style, svgElem.firstChild);
+    };
+}
+
+dynamicSvgFix();
+contactFormValidator();
